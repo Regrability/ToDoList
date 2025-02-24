c58bce1e303f5a76beb7206b2896fe1067694899
Roman Kocherov <romankocerov255@gmail.com>
2025-02-24 15:59:38 +0300

    DoTaskScreen v2

--- app/src/main/java/com/example/todolist/activity/DoTaskScreen.kt
+++ app/src/main/java/com/example/todolist/activity/DoTaskScreen.kt
@@ -44,31 +44,28 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.todolist.NavManager
 import com.example.todolist.R
+import com.example.todolist.entity.TaskLevel
+import com.example.todolist.entity.TaskInfo
 import com.example.todolist.ui.theme.FonColor
 import com.example.todolist.ui.theme.SecondColor
 import com.example.todolist.ui.theme.TextColor
 import java.time.format.TextStyle
 
 @Composable
-fun RareButton() {
+fun TaskLevelSelector(selectedLevel: TaskLevel, onLevelSelected: (TaskLevel) -> Unit) {
     var expanded by remember { mutableStateOf(false) }
-    var selectedOption by remember { mutableStateOf("DAILY") } // Начальное значение
-
-    val sortingOptions = listOf("DAILY", "WEEKLY", "MONTHLY", "YEARLY")
 
     Box(
-        modifier = Modifier
-            .fillMaxWidth(),
-        contentAlignment = Alignment.Center // Центрирует содержимое
+        modifier = Modifier.fillMaxWidth(),
+        contentAlignment = Alignment.Center
     ) {
         Button(
             onClick = { expanded = true },
-            modifier = Modifier
-                .fillMaxWidth(),
+            modifier = Modifier.fillMaxWidth(),
             shape = RoundedCornerShape(8.dp),
             colors = ButtonDefaults.buttonColors(containerColor = SecondColor)
         ) {
-            Text("Task Level: $selectedOption", color = Color.White, fontSize = 18.sp)
+            Text("Task Level: ${selectedLevel.name}", color = Color.White, fontSize = 18.sp)
         }
 
         DropdownMenu(
@@ -76,11 +73,11 @@ fun RareButton() {
             onDismissRequest = { expanded = false },
             modifier = Modifier.fillMaxWidth(0.9f)
         ) {
-            sortingOptions.forEach { option ->
+            TaskLevel.values().forEach { level ->
                 DropdownMenuItem(
-                    text = { Text(option) },
+                    text = { Text(level.name) },
                     onClick = {
-                        selectedOption = option // Обновляем выбранный вариант
+                        onLevelSelected(level)
                         expanded = false
                     }
                 )
@@ -90,139 +87,140 @@ fun RareButton() {
 }
 
 
+
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun DoTaskScreen(navManager: NavManager) {
 
     var title by remember { mutableStateOf("") }
     var info by remember { mutableStateOf("") }
-
+    var selectedLevel by remember { mutableStateOf(TaskLevel.DAILY) } // Состояние для уровня задачи
 
     Surface(modifier = Modifier.fillMaxSize(), color = FonColor) {
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(16.dp),
+            verticalArrangement = Arrangement.Top,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Spacer(modifier = Modifier.height(40.dp))
 
-            Column(
+            Image(
+                painter = painterResource(id = R.drawable.womenaim),
+                contentDescription = "womenAim",
                 modifier = Modifier
-                    .fillMaxSize()
-                    .padding(16.dp),
-                verticalArrangement = Arrangement.Top,
-                horizontalAlignment = Alignment.CenterHorizontally
-            ) {
-                Spacer(modifier = Modifier.height(40.dp)) // Отступ после кругов
+                    .size(240.dp)
+                    .clip(RoundedCornerShape(12.dp))
+            )
 
-                Image(
-                    painter = painterResource(id = R.drawable.womenaim), // Замените на ID изображения
-                    contentDescription = "womenAim",
-                    modifier = Modifier
-                        .size(240.dp) // Устанавливаем размер изображения
-                        .clip(RoundedCornerShape(12.dp)) // Округляем углы
+            Text(
+                text = "Create your task",
+                fontWeight = FontWeight.Bold,
+                fontSize = 24.sp,
+                color = TextColor
+            )
+
+            Spacer(modifier = Modifier.height(20.dp))
+
+            // Кнопка выбора уровня задачи
+            TaskLevelSelector(selectedLevel) { selectedLevel = it }
+
+            OutlinedTextField(
+                value = title,
+                onValueChange = { title = it },
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(bottom = 0.dp),
+                textStyle = androidx.compose.ui.text.TextStyle(
+                    fontSize = 20.sp,
+                    color = Color.Black
+                ),
+                singleLine = true,
+                colors = TextFieldDefaults.outlinedTextFieldColors(
+                    unfocusedBorderColor = Color.Transparent,
+                    focusedBorderColor = Color.Transparent,
+                    cursorColor = SecondColor,
+                    focusedLabelColor = TextColor,
+                ),
+                shape = RectangleShape,
+                placeholder = { Text("Title", fontSize = 20.sp, color = TextColor) }
+            )
+
+            Divider(
+                color = TextColor,
+                thickness = 1.dp,
+                modifier = Modifier.fillMaxWidth()
+            )
+
+            Spacer(modifier = Modifier.height(10.dp))
+
+            OutlinedTextField(
+                value = info,
+                onValueChange = { info = it },
+                label = { Text("Info") },
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(200.dp),
+                colors = TextFieldDefaults.outlinedTextFieldColors(
+                    focusedBorderColor = SecondColor,
+                    cursorColor = SecondColor,
+                    focusedLabelColor = TextColor,
+                ),
+                maxLines = 10,
+                singleLine = false,
+                keyboardOptions = KeyboardOptions.Default.copy(
+                    capitalization = KeyboardCapitalization.Sentences,
+                    imeAction = ImeAction.Default
                 )
+            )
 
-                Text(
-                    text = "Create your task ",
-                    fontWeight = FontWeight.Bold,
-                    fontSize = 24.sp,
-                    color = TextColor
-                )
+            Spacer(modifier = Modifier.height(30.dp))
+        }
 
-                Spacer(modifier = Modifier.height(20.dp))
-
-                RareButton()
-
-                // Поля для ввода
-
-                OutlinedTextField(
-                    value = title,
-                    onValueChange = { title = it },
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .padding(bottom = 0.dp), // Уменьшаем отступ перед линией
-
-                    textStyle = androidx.compose.ui.text.TextStyle(
-                        fontSize = 20.sp,
-                        color = Color.Black
-                    ), // Увеличенный шрифт текста
-                    singleLine = true,
-                    colors = TextFieldDefaults.outlinedTextFieldColors(
-                        unfocusedBorderColor = Color.Transparent, // Убираем стандартную границу
-                        focusedBorderColor = Color.Transparent,
-                        cursorColor = SecondColor,
-                        focusedLabelColor = TextColor,
-                    ),
-                    shape = RectangleShape, // Убираем закругления
-                    placeholder = { Text("Title", fontSize = 20.sp, color = TextColor) } // Увеличенный плейсхолдер
-                )
-
-                Divider(
-                    color = TextColor,
-                    thickness = 1.dp,
-                    modifier = Modifier.fillMaxWidth()
-                )
-
-                Spacer(modifier = Modifier.height(10.dp))
-
-
-                OutlinedTextField(
-                    value = info,
-                    onValueChange = { info = it },
-                    label = { Text("Info") },
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .height(200.dp), // Устанавливаем высоту
-                    colors = TextFieldDefaults.outlinedTextFieldColors(
-                        focusedBorderColor = SecondColor,
-                        cursorColor = SecondColor,
-                        focusedLabelColor = TextColor,
-                    ),
-                    maxLines = 10, // Позволяем многострочный ввод
-                    singleLine = false, // Разрешаем перенос строк
-                    keyboardOptions = KeyboardOptions.Default.copy(
-                        capitalization = KeyboardCapitalization.Sentences, // Автозаглавные буквы
-                        imeAction = ImeAction.Default // Стандартное поведение Enter
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(bottom = 64.dp),
+            verticalArrangement = Arrangement.Bottom,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Button(
+                onClick = {
+                    val task = TaskInfo(
+                        id = 0, // Генерируем случайный ID (в реальности лучше использовать базу данных)
+                        lvl = selectedLevel,
+                        title = title,
+                        info = info,
+                        completed = false
                     )
-                )
 
-                Spacer(modifier = Modifier.height(30.dp))
+                    // Здесь можно передавать task в базу данных или список задач
+                    println("Создана задача: $task")
 
-
-
-            }
-
-            // Кнопка, закрепленная внизу экрана
-            Column(
+                    navManager.navigateToMainScreen()
+                },
                 modifier = Modifier
-                    .fillMaxSize()
-                    .padding(bottom = 64.dp),
-                verticalArrangement = Arrangement.Bottom,
-                horizontalAlignment = Alignment.CenterHorizontally
+                    .fillMaxWidth()
+                    .padding(horizontal = 32.dp),
+                shape = RoundedCornerShape(8.dp),
+                colors = ButtonDefaults.buttonColors(containerColor = SecondColor)
             ) {
-                Button(
-                    onClick = {
-
-                        navManager.navigateToMainScreen()
-                    },
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .padding(horizontal = 32.dp),
-                    shape = RoundedCornerShape(8.dp),
-                    colors = ButtonDefaults.buttonColors(containerColor = SecondColor)
-                ) {
-                    Text(text = "CREATE", color = Color.White, fontSize = 18.sp)
-                }
-                Button(
-                    onClick = {
-
-                        navManager.goBack()
-                    },
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .padding(horizontal = 32.dp),
-                    shape = RoundedCornerShape(8.dp),
-                    colors = ButtonDefaults.buttonColors(containerColor = SecondColor)
-                ) {
-                    Text(text = "Go Back", color = Color.White, fontSize = 18.sp)
-                }
-
+                Text(text = "CREATE", color = Color.White, fontSize = 18.sp)
             }
 
+            Button(
+                onClick = {
+                    navManager.goBack()
+                },
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(horizontal = 32.dp),
+                shape = RoundedCornerShape(8.dp),
+                colors = ButtonDefaults.buttonColors(containerColor = SecondColor)
+            ) {
+                Text(text = "Go Back", color = Color.White, fontSize = 18.sp)
+            }
+        }
     }
 }
